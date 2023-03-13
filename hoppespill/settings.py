62b4e06e9a5fd92aa89b2c10c97fe0c29cecdd71
akselwe <112179914+akselwe@users.noreply.github.com>
2023-03-13 20:50:13 +0100

    Add files via upload

---
+++ hoppespill/settings.py
@@ -0,0 +1,18 @@
+#color 
+WHITE = (255, 255, 255)
+LIGHTBLUE = (200, 228, 230)
+BLUE = (12, 12, 117)
+
+#game constants
+playerSize = 60
+lineWidth = 20
+MaxPlatforms = 10
+start = 100
+l = [1,2,3,4]
+lvls = [i * 100 for i in l]
+fps = 60
+running = True
+
+leftScore = 0
+rightScore = 0
+scores = [leftScore, rightScore]
